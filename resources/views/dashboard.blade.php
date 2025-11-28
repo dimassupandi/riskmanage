da10b21e9834c869158ed0ac7b362148b3206620
dimassupandi <160721651+dimassupandi@users.noreply.github.com>
2025-11-28 09:15:26 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
           <div class="col-8">
             <div class="numbers">
-              <p class="text-sm mb-0 text-capitalize font-weight-bold">User</p>
+              <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengguna</p>
               <h5 class="font-weight-bolder mb-0">{{ $user }}</h5>
             </div>
           </div>
